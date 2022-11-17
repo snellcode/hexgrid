1eca5cecd4eef9e093bcb60bca2fdf3fab799a95
Phil Snell <snellcode@gmail.com>
2022-11-16 22:50:47 -0500

    add island demo

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -5,6 +5,7 @@ const Home = () => {
     <div class="app-route container">
       <h1>Hexagons!</h1>
       <h2>Choose from Examples Menu</h2>
+      <p>Or try <a href="/island">this</a> one.</p>
     </div>
   );
 };
